a391a3731ba9a5139903857ae993200c31589035
Igor Laur <igor.laur2@gmail.com>
2026-01-13 11:15:24 -0300

    Remove imports e variáveis não utilizados para build limpo

--- discretta-hub/src/App.tsx
+++ discretta-hub/src/App.tsx
@@ -1,9 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import MinhaFuncao from './pages/MinhaFuncao';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 

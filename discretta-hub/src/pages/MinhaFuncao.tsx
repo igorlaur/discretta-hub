ad544968fe51d225b8709e0b30a9c430e9e3679c
Igor Laur <igor.laur2@gmail.com>
2026-01-13 11:11:45 -0300

    Adiciona src/pages/MinhaFuncao.tsx para corrigir build em produção

---
+++ discretta-hub/src/pages/MinhaFuncao.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MinhaFuncao = () => {
+  return (
+    <div>
+      <h1>Minha Função</h1>
+      <p>Página de exemplo para produção.</p>
+    </div>
+  );
+};
+
+export default MinhaFuncao;
